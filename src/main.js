bf61000a9b640bb399b5e87515d6c67ad81aad54
ColeyG <cjgeerts@gmail.com>
2020-01-20 22:15:46 -0500

    Loading Animation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import '../styles/main.scss';
 import 'babel-polyfill';
 import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch';
+import anime from 'animejs/lib/anime.es.js';
+
+const bezier = '.64,1.53,1,1.07';
+
+anime({
+  targets: '.load-ball',
+  keyframes: [{ rotate: 45 }, { rotate: 90 }, { rotate: 135 }, { rotate: 180 }],
+  loop: true,
+  easing: `cubicBezier(${bezier})`,
+});
+
+anime({
+  targets: '.load-body',
+  keyframes: [{ rotate: 45 }, { rotate: 90 }, { rotate: 135 }, { rotate: 180 }],
+  loop: true,
+  easing: `cubicBezier(${bezier})`,
+});
 
 async function fetchData(resource) {
   const result = await fetch(`https://swapi.co/api/${resource}`, { method: 'GET', mode: 'cors' })
@@ -14,6 +31,7 @@ async function fetchData(resource) {
 const initialize = () => {
   fetchData('people/').then((data) => {
     console.log(data.results);
+    document.querySelector('.loading').style.opacity = 0;
   });
 };
 
