a9009007e51b9c92d7530ca84c1b0f4df0b67c30
ColeyG <cjgeerts@gmail.com>
2020-01-20 23:42:00 -0500

    Adding Character Cards

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'whatwg-fetch';
 import anime from 'animejs/lib/anime.es.js';
 
 const bezier = '.64,1.53,1,1.07';
+const content = document.querySelector('.content-cards');
 
 anime({
   targets: '.load-ball',
@@ -28,10 +29,48 @@ async function fetchData(resource) {
   return result;
 }
 
+const createAndClassElement = (className) => {
+  const element = document.createElement('div');
+  element.className = className;
+  return element;
+};
+
+const addText = (element, text) => {
+  element.append(document.createTextNode(text));
+};
+
+const characterSelect = () => {
+  console.log('zxcv');
+};
+
+const addCharacterCard = (name, description) => {
+  const link = document.createElement('a');
+  link.className = 'card-link';
+  link.addEventListener('click', characterSelect, false);
+  const card = createAndClassElement('card');
+  const cardContent = createAndClassElement('card-content');
+  const media = createAndClassElement('media');
+  const mediaContent = createAndClassElement('media-content');
+  const title = document.createElement('p');
+  title.className = 'title is-4';
+  addText(title, name);
+  mediaContent.append(title);
+  media.append(mediaContent);
+  const contenta = createAndClassElement('content');
+  addText(contenta, description);
+  cardContent.append(media);
+  cardContent.append(contenta);
+  card.append(cardContent);
+  link.append(card);
+  content.append(link);
+};
+
 const initialize = () => {
   fetchData('people/').then((data) => {
-    console.log(data.results);
     document.querySelector('.loading').style.opacity = 0;
+    data.results.forEach((character) => {
+      addCharacterCard(character.name, 'asdf');
+    });
   });
 };
 
